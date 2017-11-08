495a8987032e15cabd51ca1ae3f0cb92dd5b27ee
Brandon Leeds <brandonleeds@gmail.com>
2017-11-08 01:22:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,46 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+def move(board, index, character)
+  board[index] = character
+end
+
+def position_taken?(board, index)
+  return board[index] != " "
+end
+
+def valid_move?(board, index)
+  return index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Enter a position 1 - 9"
+  input = gets.chomp
+  index = input_to_index(index)
+  if valid_move?(board, index)
+    move(board, index, "X")
+    puts display_board(board)
+  else
+    turn(board)
+  end
+end
